27e1d7db1d0eb51a6f4fa329d02439ba130a5413
Christopher Chua <chuachristopher@hotmail.com>
2020-06-26 21:07:18 +0800

    Render an inventory index with Bulma CSS boxes

--- routes/inventory.js
+++ routes/inventory.js
@@ -10,7 +10,16 @@ const router = express.Router();
 router.get('/inventory', 
     requireAuth,
     async (req, res) => {
-        res.render(inventoryIndexTemplate);
+        try {
+            let [rows] = await db.pool.query('SELECT * FROM items ORDER BY last_updated DESC');
+            if (rows.length > 0) { // found at least one item
+                res.render(inventoryIndexTemplate, { rows });
+            } else {
+                res.render(inventoryIndexTemplate, { rows: [] });
+            }
+        } catch (error) {
+            throw error;
+        }
 });
 
 
--- schema.sql
+++ schema.sql
@@ -13,8 +13,15 @@ CREATE TABLE items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     size VARCHAR(100),
-    num_count DECIMAL(8, 2), -- can hold up to 999,999.99
+    num_count DECIMAL(8, 2), -- can hold up to number 999,999.99
     description VARCHAR(100),
     image_link VARCHAR(255),
     last_updated TIMESTAMP DEFAULT NOW()
 );
+
+INSERT INTO items (name, size, num_count, description)
+VALUES
+    ('Milk', '2L', 4, 'Milk for coffee'),
+    ('Sausages', '6 pack', 1, 'Sausages for hot dogs'),
+    ('Coffee beans', '1kg', 3, 'Coffee beans')
+;
